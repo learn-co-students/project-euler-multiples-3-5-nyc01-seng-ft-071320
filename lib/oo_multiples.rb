6698f605875df6d290e254a39e57894534449493
humorous-storage-7521 <rmwerner@umich.edu>
2021-02-11 20:50:38 -0600

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+class Multiples 
+
+    def initialize(num) 
+        @num = num
+    end
+
+    def collect_multiples
+        count = 3
+        arr = []
+        while count < @num do 
+            if count % 3 == 0 || count % 5 == 0
+                arr << count
+            end
+            count += 1
+        end
+        return arr
+    end
+
+    def sum_multiples
+        sum = 0
+        self.collect_multiples.each {|n| sum += n}
+        return sum
+    end
+
+end
